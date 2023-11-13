64cf20bd884ff54a49f81a7d48bbd89656d57433
Daniil <pefilev007@outlook.com>
2023-11-13 13:03:12 +0100

    functions have been chanched

--- minsort_inplace/main.cpp
+++ minsort_inplace/main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <vector>
 
-int min_arr(const std::vector<int>& arr){
-    int min = arr[0];
+std::vector<int> min_arr(const std::vector<int>& arr){
+    std::vector<int> return_min_arr (2);
+    return_min_arr[0] = arr[0];
+
     for (int i = 0; i < arr.size(); i++){
-        if (min > arr[i]){
-            min = arr[i];
-        } else if (min == arr[i]){
-            min = arr[i];
+        if (return_min_arr[0] > arr[i]){
+            return_min_arr[0] = arr[i];
+            return_min_arr[1] = i;
+        } else if (return_min_arr[0] == arr[i]){
+            return_min_arr[0] = arr[i];
+            return_min_arr[1] = i;
         }
     }
-    return min;
+    return return_min_arr;
 }
 
 void minsort (std::vector<int>& arr){
-    int min = min_arr(arr);
+    int min = min_arr(arr)[0];
+    int min_index = min_arr(arr)[1];
+
     int temp;
     if (arr[0] != min){
         temp = arr[0];
         arr[0] = min;
+        arr[min_index] = temp;
     }
 }
 
@@ -31,7 +38,7 @@ void print_array (const std::vector<int>& arr){
 }
 
 int main(){
-    std::vector<int> arr ={9,1,2,0,4,5};
+    std::vector<int> arr ={9,1,62,0,4,5};
 
     print_array(arr);
     minsort(arr);
