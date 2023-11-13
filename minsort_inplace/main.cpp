7821f79a952284ce396f1ff2a5e9745a6d30f1a8
Daniil <pefilev007@outlook.com>
2023-11-13 01:11:00 +0100

    print function

--- minsort_inplace/main.cpp
+++ minsort_inplace/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <vector>
 
+void print_array (const std::vector<int>& arr){
+    for(const int numb: arr){
+        std::cout << numb << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main(){
     std::vector<int> arr ={9,1,2,0,4,5};
+
+    print_array(arr);
+
     return 0;
 }
